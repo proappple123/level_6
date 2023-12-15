583206c18b492e6cb907a9fbbe4fcd76092393f7
yongzhi <yongzhi@bixtech.co>
2023-12-15 21:23:21 +0800

    Put add-product-form inside a modal

--- src/level_6_frontend/src/index.js
+++ src/level_6_frontend/src/index.js
@@ -2,10 +2,10 @@ import { level_6_backend } from "../../declarations/level_6_backend";
 import { AuthClient } from "@dfinity/auth-client";
 
 let isLoggedIn = false;
-let principalId = getCookie('principalId');
+let principalId = getCookie("principalId");
 
 function loadHomePage() {
-  const welcomeDiv = document.getElementById('welcome-message');
+  const welcomeDiv = document.getElementById("welcome-message");
   welcomeDiv.innerHTML = `
   <div id="header">
   <h1>Welcome to DFINITY E-Commerce</h1>
@@ -16,13 +16,15 @@ function loadHomePage() {
   <div id="principal-id">Principal ID: ${principalId}</div>
   `;
   const contentDiv = document.getElementById("content");
-  principalId = getCookie('principalId');
-  isLoggedIn = principalId !== '' && principalId !== null;
+  principalId = getCookie("principalId");
+  isLoggedIn = principalId !== "" && principalId !== null;
   contentDiv.innerHTML = `
     <button id="buy-button">Buy Cart</button>
     </div>`;
 
-  document.getElementById("buy-button").addEventListener("click", buyCurrentCart);
+  document
+    .getElementById("buy-button")
+    .addEventListener("click", buyCurrentCart);
   document.getElementById("login-button").addEventListener("click", login);
   document.getElementById("logout-button").addEventListener("click", logout);
 
@@ -37,7 +39,7 @@ async function login() {
       const identity = authClient.getIdentity();
       principalId = identity.getPrincipal().toString();
       console.log("Logged in as:", principalId);
-      setCookie('principalId', principalId, 7);
+      setCookie("principalId", principalId, 7);
       isLoggedIn = true;
       loadHomePage();
     },
@@ -47,25 +49,25 @@ async function login() {
   });
 }
 
-async function logout(){
+async function logout() {
   const authClient = await AuthClient.create();
   authClient.logout();
-  eraseCookie('principalId');
-  principalId = '';
+  eraseCookie("principalId");
+  principalId = "";
   isLoggedIn = false;
   console.log("Logged out");
   location.reload();
 }
 
-async function updateLoginButton(){
+async function updateLoginButton() {
   const loginBtn = document.getElementById("login-button");
   const logoutBtn = document.getElementById("logout-button");
-  if (isLoggedIn){
-    loginBtn.style.display = 'none';
-    logoutBtn.style.display = 'block';
+  if (isLoggedIn) {
+    loginBtn.style.display = "none";
+    logoutBtn.style.display = "block";
   } else {
-    loginBtn.style.display = 'block';
-    logoutBtn.style.display = 'none';
+    loginBtn.style.display = "block";
+    logoutBtn.style.display = "none";
   }
 }
 
@@ -164,28 +166,30 @@ async function loadViewCart() {
   }
 }
 
-async function buyCurrentCart(){
-  try{
+async function buyCurrentCart() {
+  try {
     const cartItems = await level_6_backend.buyCurrentCart();
     alert(cartItems);
-  } catch(err){
+  } catch (err) {
     alert("Error loading cart");
-  } 
+  }
 }
 
-function openNav(){
-document.getElementById("mySidenav").style.width = '250px';
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.padding = "20px";
 }
 
-function closeNav(){
-  document.getElementById("mySidenav").style.width = '0';
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.padding = "0";
 }
 
 function setCookie(name, value, days) {
   let expires = "";
   if (days) {
     let date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
@@ -193,24 +197,51 @@ function setCookie(name, value, days) {
 
 function getCookie(name) {
   let nameEQ = name + "=";
-  let ca = document.cookie.split(';');
+  let ca = document.cookie.split(";");
   for (let i = 0; i < ca.length; i++) {
     let c = ca[i];
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    while (c.charAt(0) === " ") c = c.substring(1, c.length);
     if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
 
 function eraseCookie(name) {
-  document.cookie = name + '=; Max-Age=-99999999;';
+  document.cookie = name + "=; Max-Age=-99999999;";
 }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 document.getElementById("open-btn").addEventListener("click", openNav);
 document.getElementById("close-btn").addEventListener("click", closeNav);
 document.getElementById("nav-home").addEventListener("click", loadHomePage);
-document.getElementById("nav-products").addEventListener("click", loadProductPage);
+document
+  .getElementById("nav-products")
+  .addEventListener("click", loadProductPage);
 document.getElementById("nav-cart").addEventListener("click", loadViewCart);
 
 loadHomePage();
